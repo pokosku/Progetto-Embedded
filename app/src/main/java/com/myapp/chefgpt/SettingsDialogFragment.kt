43d4e6ab18f12f97e5c7fdf18ec06e67a4877d2c
LUCA9725 <lucatonin2003@gmail.com>
2025-05-17 18:37:22 +0200

    ora si può uscire dalle impostazioni

--- app/src/main/java/com/myapp/chefgpt/SettingsDialogFragment.kt
+++ app/src/main/java/com/myapp/chefgpt/SettingsDialogFragment.kt
@@ -1,30 +1,45 @@
 package com.myapp.chefgpt
 
-import android.app.Dialog
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.FrameLayout
+import android.widget.LinearLayout
 import androidx.fragment.app.DialogFragment
 
 class SettingsDialogFragment : DialogFragment() {
-
-    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        val dialog = super.onCreateDialog(savedInstanceState)
-        // Sfondo trasparente per il dialog
-        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
-        return dialog
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Layout del dialog
         return inflater.inflate(R.layout.fragment_settings_dialog, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val backgroundLayout = view.findViewById<FrameLayout>(R.id.backgroundLayout)
+        val contentLayout = view.findViewById<LinearLayout>(R.id.contentLayout)
+        val okButton = view.findViewById<Button>(R.id.okButton)
+
+        //Chiudi se clicchi FUORI dal contenuto
+        backgroundLayout.setOnClickListener {
+            dismiss()
+        }
+        //Ignora i clic SUL contenuto (altrimenti lo chiuderebbe anche lì)
+        contentLayout.setOnClickListener {
+            // Non fare nulla
+        }
+        //Chiudi cliccando OK
+        okButton.setOnClickListener {
+            dismiss()
+        }
     }
 
     override fun onStart() {
@@ -34,7 +49,6 @@ class SettingsDialogFragment : DialogFragment() {
             ViewGroup.LayoutParams.MATCH_PARENT
         )
         // Sfondo semi-trasparente nero (overlay)
-        dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.parseColor("#99000000")))
-        // Permetti chiusura cliccando fuori dal dialog da implementare
+        dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
     }
 }
