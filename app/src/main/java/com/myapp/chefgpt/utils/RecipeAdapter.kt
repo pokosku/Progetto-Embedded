7d8bcd5a5e04c3717b477fbb7f8715bdd46dae5a
Dercombi <daniele.riolmirossetto@gmail.com>
2025-05-19 09:39:57 +0200

    cambiato il bottone in favorite mettendoci un'immagine.

--- app/src/main/java/com/myapp/chefgpt/utils/RecipeAdapter.kt
+++ app/src/main/java/com/myapp/chefgpt/utils/RecipeAdapter.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
@@ -22,7 +23,7 @@ class RecipeAdapter(private var recipeList : List<Recipe> =
     class RecipeViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         private val recipeNameTextView: TextView = itemView.findViewById(R.id.recipe_name)
         private val creationDateTextView: TextView = itemView.findViewById(R.id.creation_date)
-        private val deleteButton: Button = itemView.findViewById(R.id.delete_recipe_btn)
+        private val deleteButton: ImageButton = itemView.findViewById(R.id.delete_recipe_btn)
 
         fun bind(recipe: Recipe, mRecipeViewModel: RecipeViewModel){
             recipeNameTextView.text = recipe.name
