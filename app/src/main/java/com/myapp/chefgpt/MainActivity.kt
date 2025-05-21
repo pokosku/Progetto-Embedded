9a5b60593849d3db07c5f461d2ac29505114c7fb
LUCA9725 <lucatonin2003@gmail.com>
2025-05-22 00:58:55 +0200

    bottone back migliorato. light/dark/system mode. languages da implementare. colori della status bar trasparenti

--- app/src/main/java/com/myapp/chefgpt/MainActivity.kt
+++ app/src/main/java/com/myapp/chefgpt/MainActivity.kt
@@ -1,16 +1,34 @@
 package com.myapp.chefgpt
 
+import android.content.Context
 import android.os.Bundle
 import android.content.Intent
 import android.widget.Button
 import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import android.content.res.Configuration
+import java.util.Locale
 
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        //le preferenze dell'app
+        val prefs = getSharedPreferences("app_preferences", Context.MODE_PRIVATE)
+        //tema chiaro o scuro
+        val theme = prefs.getInt("selected_theme", AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+        AppCompatDelegate.setDefaultNightMode(theme)
+        //lingua
+        val langCode = prefs.getString("selected_language", "en") ?: "en"
+        val locale = Locale(langCode)
+        Locale.setDefault(locale)
+        val config = Configuration()
+        config.setLocale(locale)
+        this.resources.updateConfiguration(config, this.resources.displayMetrics)
+
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
 
         val toImagePredictionButton = findViewById<Button>(R.id.toImagePredictionButton)
